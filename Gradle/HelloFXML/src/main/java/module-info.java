cf6d9df8298c3324d735e73c29a89df413c09b22
Abhinay Agarwal <abhinay.agarwal@gluonhq.com>
2019-11-21 10:32:52 +0000

    Remove Gradle samples (#27)

--- Gradle/HelloFXML/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module hellofxml {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens hellofx to javafx.fxml;
-
-    exports hellofx;
-}
--- Gradle/HelloGluon/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module hellogluon {
-    requires javafx.controls;
-    requires com.gluonhq.attach.display;
-    requires com.gluonhq.attach.storage;
-    requires com.gluonhq.attach.util;
-    requires com.gluonhq.charm.glisten;
-
-    exports com.gluonhq.hello;
-}
